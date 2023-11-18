4759afcde534cb3647a508686ae202f7948b5a73
ginx836 <141227582+ginx836@users.noreply.github.com>
2023-11-18 10:57:27 +0100

    Intégration de la page 404
    Intégration de la page 404.
    Nettoyage des fichiers SCSS

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom';
+import '../styles/errorpage.scss';
+
 
 function ErrorPage() {
   return (
-    <div>
+    <div className='errorpage__container'>
       <h1>404</h1>
-      <p>Page not found</p>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   )
