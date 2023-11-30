84699953a8be6d9f59523bf5cbef2506270ea080
ginx836 <141227582+ginx836@users.noreply.github.com>
2023-11-30 14:05:26 +0100

    Update caroussel.jsx
    Modification du composant

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -29,24 +29,24 @@ const Carousel = ({ images }) => {
     setCurrentImage(currentImage === 0 ? length - 1 : currentImage - 1)
   }
 
+  /**
+   * Récupère l'image actuelle
+   */
+  const currentPicture = images[currentImage]
+
   return (
     <div className="slider">
-      {images.map((picture, index) => {
-        const isActive = index === currentImage
-        return (
-          <div key={index} className={isActive ? 'slide active' : 'slide'}>
-            {isActive && <img src={picture} alt="Logement" />}
-          </div>
-        )
-      })}
-      {length > 1 ? (
+      <div className={length > 1 ? 'slide active' : 'slide'}>
+        {currentPicture && <img src={currentPicture} alt="Logement" />}
+      </div>
+      {length > 1 && (
         <>
           <ControlButtons onPrev={prevImage} onNext={nextImage} />
           <div className="counter">
             {currentImage + 1}/{length}
           </div>
         </>
-      ) : null}
+      )}
     </div>
   )
 }
